19daacedca775ca8ff41cee5bb231b07d435fea4
JackedPro <129242133+JackedPro@users.noreply.github.com>
2025-06-16 19:15:12 +1000

    Update register.php

--- back/register.php
+++ back/register.php
@@ -3,6 +3,6 @@ require 'db.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 $db->exec("INSERT INTO users (username, password) VALUES ('$username', '$password')");
-setcookie("user", $username, time() + 3600); // Cookie реализовано здесь
+setcookie("user", $username, time() + 3600);
 header("Location: welcome.php");
 ?>
