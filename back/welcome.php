adb655676efc08443b19f785a5bea1672b1339f1
JackedPro <129242133+JackedPro@users.noreply.github.com>
2025-06-16 19:25:39 +1000

    Update welcome.php

--- back/welcome.php
+++ back/welcome.php
@@ -6,3 +6,14 @@ if (!isset($_COOKIE['user'])) {
 $user = htmlspecialchars($_COOKIE['user']);
 echo "Привет, $user";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Добро пожаловать</title>
+</head>
+<body>
+    <h2>Привет, <?php echo $user; ?></h2>
+    <p>Вы успешно авторизовались.</p>
+</body>
+</html>
