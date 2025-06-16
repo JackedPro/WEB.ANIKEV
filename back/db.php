94554e4a444b48fe522f96c60baf1ea34ad92f8e
JackedPro <129242133+JackedPro@users.noreply.github.com>
2025-06-16 19:39:49 +1000

    del

--- back/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = new PDO('sqlite:users.db');
-$db->exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
-?>
--- back/register.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db.php';
-$username = $_POST['username'];
-$password = $_POST['password'];
-$db->exec("INSERT INTO users (username, password) VALUES ('$username', '$password')");
-setcookie("user", $username, time() + 3600);
-header("Location: welcome.php");
-?>
