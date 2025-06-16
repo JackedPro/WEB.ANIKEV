5138035e55832b9702744333390b6cee3e5eb050
JackedPro <129242133+JackedPro@users.noreply.github.com>
2025-06-16 19:13:54 +1000

    login.php

--- back/login.php
+++ back/login.php
@@ -4,7 +4,7 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $result = $db->query("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
 if ($result->fetch()) {
-    setcookie("user", $username, time() + 3600); // Cookie реализовано здесь
+    setcookie("user", $username, time() + 3600);
     header("Location: welcome.php");
 } else {
     echo "Invalid credentials";
